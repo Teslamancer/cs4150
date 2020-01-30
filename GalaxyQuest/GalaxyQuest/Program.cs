5b3ff3f5a9b499b5fc876890256369240db1d872
u0916776@eng.utah.edu <u0916776@eng.utah.edu>
2020-01-29 20:48:39 -0700

    This is closer. Still getting an answer wrong though.

--- GalaxyQuest/GalaxyQuest/Program.cs
+++ GalaxyQuest/GalaxyQuest/Program.cs
@@ -38,7 +38,7 @@ namespace GalaxyQuest
             Star.distance = int.Parse(tokens[0]);
             int numStars = int.Parse(tokens[1]);
             //Stack<Star> candidateGalaxy = new Stack<Star>();
-            Stack<Star> PU = new Stack<Star>();
+            List<Star> PU = new List<Star>();
             
             //if(numStars % 2 != 0)
             //{
@@ -50,31 +50,25 @@ namespace GalaxyQuest
                 string star = Console.ReadLine();
                 string[] starCoordinates = star.Split(' ');
 
-                PU.Push(new Star(int.Parse(starCoordinates[0]),int.Parse(starCoordinates[1])));
+                PU.Add(new Star(int.Parse(starCoordinates[0]),int.Parse(starCoordinates[1])));
             }
-            List<Star> immutablePU = new List<Star>(PU);
-            Star CandidateStar = getMajorityGalaxyStarCandidate(PU);
+            //Star CandidateStar = getMajorityGalaxyStarCandidate(PU);
             //Console.WriteLine("Candidate Star - x=" + CandidateStar.x + " y=" + CandidateStar.y);
 
-            hasMajorityGalaxy(CandidateStar, immutablePU);
-
-            Console.Read();
+            //hasMajorityGalaxy(CandidateStar, PU);
+            getMajorityGalaxyStarCandidate(PU, PU);
+            //Console.Read();
         }
 
         static void hasMajorityGalaxy(Star Candidate, List<Star> PU)
         {
-            Stack<Star> toReturn = new Stack<Star>();
-            if(Candidate == null)
-            {
-                Console.WriteLine("NO");
-                return;
-            }
+            List<Star> toReturn = new List<Star>();            
             //toReturn.Push(Candidate);
             foreach (Star s in PU)
             {
                 if (Candidate.inGalaxy(s))
                 {
-                    toReturn.Push(s);
+                    toReturn.Add(s);
                 }
             }
             if(toReturn.Count > PU.Count / 2)
@@ -87,30 +81,57 @@ namespace GalaxyQuest
             }
         }
 
-        static Star getMajorityGalaxyStarCandidate(Stack<Star> candidateGalaxy)
+        static Star getMajorityGalaxyStarCandidate(List<Star> candidateGalaxy, List<Star> PU)
         {
             int loopCount = candidateGalaxy.Count - 1;
-            Stack<Star> toReturn = new Stack<Star>();
+            List<Star> toReturn = new List<Star>();
+            Star y = null;
             if (candidateGalaxy.Count == 0)
                 return null;
             else if (candidateGalaxy.Count == 1)
-                return candidateGalaxy.Pop();
-            if (candidateGalaxy.Count % 2 != 0)
+                return candidateGalaxy[0];
+            //else if(candidateGalaxy.Count == 2)
+            //{
+            //    //candidateGalaxy.Pop();
+            //    return candidateGalaxy.Pop();
+            //}
+            else
             {
-                loopCount--;
-                toReturn.Push(candidateGalaxy.Pop());
-            }
-            for(int i=0;i<loopCount;i+=2)
-            {
-                Star a = candidateGalaxy.Pop();
-                Star b = candidateGalaxy.Pop();
-                if (a.inGalaxy(b))
+                if (candidateGalaxy.Count % 2 != 0)
                 {
-                    toReturn.Push(a);//TODO: Fix this, as pushing both on can form an infinite loop, but only pushing one can cause a false negative
-                    toReturn.Push(b);
+                    loopCount--;
+                    //toReturn.Add(candidateGalaxy[0]);
+                    y = candidateGalaxy[candidateGalaxy.Count - 1];
+                }            
+                for(int i=0;i<=loopCount;i+=2)
+                {
+                    Star a = candidateGalaxy[i];
+                    Star b = candidateGalaxy[i + 1];
+                    if (a.inGalaxy(b))
+                    {
+                        //toReturn.Push(a);//TODO: Fix this, as pushing both on can form an infinite loop, but only pushing one can cause a false negative
+                        toReturn.Add(b);
+                    }
                 }
+                Star x = getMajorityGalaxyStarCandidate(toReturn, PU);
+
+                if(x == null)
+                {
+                    if (PU.Count % 2 != 0)
+                    {
+                        hasMajorityGalaxy(y, PU);
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+                else
+                {
+                    hasMajorityGalaxy(x, PU);
+                }
+                return null;
             }
-            return getMajorityGalaxyStarCandidate(toReturn);
         }
                 
     }
